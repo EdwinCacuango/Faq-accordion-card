b4cefdc03d88fc8c5b61085dc22fdf11b094ef6c
Edwin Cacuango <edwin-fernando@hotmail.es>
2021-11-24 18:25:14 -0500

    adding first functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const btnQuestion = document.querySelectorAll(".btn-question");
+const answer=document.querySelectorAll(".answer")
+
+const selectedBtn=(event)=>{
+    const selectTarget=event.target.id
+   }
+
+btnQuestion.forEach((btn) => {
+  btn.addEventListener("click", (selectedBtn)
+  );
+});
+
+
