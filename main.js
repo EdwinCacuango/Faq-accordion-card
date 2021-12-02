737500f6c5535a34220a514f1585eee7216d3410
Edwin Cacuango <edwin-fernando@hotmail.es>
2021-12-02 13:14:01 -0500

    add display to answer div

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ console.log(btnQuestions);
 btnQuestions.forEach((btn)=>{
     btn.addEventListener('click',(e)=>{
         btn.classList.toggle('open');
+        btn.nextElementSibling.classList.toggle('open-answer');
     })
     
 });
